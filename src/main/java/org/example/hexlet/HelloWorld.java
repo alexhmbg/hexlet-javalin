64f0f6e5972a14aba28859a17e05137cf2472950
alexhmbg <uplink205@gmail.com>
2024-03-05 11:57:42 +0200

    update HelloWorld.java

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,20 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");;
+            ctx.result("Hello, " + name + "!");
+        });
+
+        app.get("/courses/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+
+        app.get("users/{id}/post/{postId}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+            ctx.result("User Post ID: " + ctx.pathParam("postId"));
+        });
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
